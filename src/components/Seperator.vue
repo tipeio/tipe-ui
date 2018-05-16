6514bad7db12bc224be33a38164ad4c612d4a0a2
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-15 19:43:37 -0700

    seperator

---
+++ src/components/Seperator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :data-tipe-ui="$options.name" />
+</template>
+
+<script>
+export default {
+  name: 'TipeSeperator'
+}
+</script>
+
+<style scoped>
+[data-tipe-ui='TipeSeperator'] {
+  height: 1px;
+  background-color: var(--gray-light);
+}
+</style>
