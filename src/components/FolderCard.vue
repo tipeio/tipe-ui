c7245036fac880d8d4a8d22b3cc4b28f06bc5a89
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-16 16:43:55 -0700

    Folder Card Component: in progress folder card component

---
+++ src/components/FolderCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div 
+    :data-tipe-ui="$options.name" 
+    class="card">
+    <div class="title">
+      <div>꠸</div>
+      <p><slot /></p>
+    </div>
+    <div class="dots">⁞</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FolderCard'
+}
+</script>
+
+
+<style lang='postcss' scoped>
+.card {
+  border: solid 1px #e9eff4;
+  border-radius: 5px;
+  background-color: #fff;
+  height: 53px;
+  width: 263px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & .title {
+    display: flex;
+    align-items: center;
+    margin-left: 15px;
+
+    & p {
+      color: #323c47;
+      font-size: 1.25rem;
+      margin-left: 15px;
+    }
+  }
+
+  & .dots {
+    justify-self: end;
+    margin-right: 15px;
+  }
+}
+</style>
