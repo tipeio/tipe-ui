0b76a79e0613f41dc4e81b2057caa6d917929d69
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-15 19:41:11 -0700

    icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :class="size"
+    :data-tipe-ui="$options.name"
+  />
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+
+const sizes = ['small', 'medium', 'large']
+
+export default {
+  name: 'TipeIcon',
+  props: {
+    size: vueTypes.oneOf(sizes).def('medium')
+  }
+}
+</script>
+
+<style scoped>
+[data-tipe-ui='TipeIcon'] {
+  background-color: red;
+}
+
+[data-tipe-ui='TipeIcon'].small {
+  height: 1rem;
+  width: 1rem;
+}
+
+[data-tipe-ui='TipeIcon'].medium {
+  height: 2rem;
+  width: 2rem;
+}
+
+[data-tipe-ui='TipeIcon'].large {
+  height: 3rem;
+  width: 3rem;
+}
+</style>
