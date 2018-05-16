c3e0c2e39873dc0ae884c92b6b45974a383a4a76
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-15 19:54:59 -0700

    topbar

---
+++ src/compositions/Topbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :data-tipe-ui="$options.name">
+    <tipe-search class="search"/>
+    <tipe-profile-image
+      class="menu"
+      @click="dosomething"
+    />
+    <tipe-seperator class="seperator"/>
+  </div>
+</template>
+
+<script>
+import TipeSearch from '../components/Search'
+import TipeProfileImage from '../components/ProfileImage'
+import TipeSeperator from '../components/Seperator'
+
+export default {
+  name: 'TipeTopbar',
+  components: {
+    TipeSearch,
+    TipeProfileImage,
+    TipeSeperator
+  },
+  methods: {
+    dosomething() {
+      console.log('haha')
+    }
+  }
+}
+</script>
+
+<style scoped>
+[data-tipe-ui='TipeTopbar'] {
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-areas:
+    'search menu'
+    'seperator seperator';
+  grid-template-columns: 1fr auto;
+  grid-template-rows: 1fr 1px;
+  align-items: center;
+  padding: 0.4rem 1.4rem 0 1.4rem;
+  box-sizing: border-box;
+}
+
+.search {
+  grid-area: search;
+}
+
+.menu {
+  grid-area: menu;
+}
+
+.seperator {
+  margin-top: 1.4rem;
+  grid-area: seperator;
+}
+</style>
