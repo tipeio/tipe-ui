4e35ae557266f7e27cf9cfc1be10f41b12582920
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-15 20:10:17 -0700

    Button component: added postcss to component but is in progress

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-cssnext': {},
-    cssnano: {}
+    cssnano: {},
+    'postcss-mixins': {}
   }
 }
--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,58 +1,58 @@
 import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 
-import Btn from '../src/components/Button.vue'
+import TipeButton from '../src/components/Button.vue'
 
 storiesOf('Button', module)
   .add('Fill/Default Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn @click="action">Fill Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Outline Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn type="outline" @click="action">Outline Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Small Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn size="small" @click="action">Small Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Medium Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn size="medium" @click="action">Medium Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Large Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn size="large" @click="action">Large Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Danger Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn btnStyle="danger" @click="action">Danger Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Dark Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template: '<btn btnStyle="dark" dark @click="action">Dark Button</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Danger Outline Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template:
       '<btn btnStyle="danger" type="outline" @click="action">Danger Outline</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Dark Outline Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template:
       '<btn btnStyle="dark" type="outline" @click="action">Dark Outline</btn>',
     methods: { action: action('clicked') }
   }))
   .add('Small Outline Danger Button', () => ({
-    components: { Btn },
+    components: { TipeButton },
     template:
       '<btn btnStyle="danger" size="small" type="outline" @click="action">Small Outline Danger</btn>',
     methods: { action: action('clicked') }
