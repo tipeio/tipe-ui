f0161e7d106ecb8cc4cfc0ef4e53dbe99525f01a
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-16 22:04:52 -0700

    icons with svgs

--- stories/components/Icon.stories.js
+++ stories/components/Icon.stories.js
@@ -2,15 +2,44 @@ import { storiesOf } from '@storybook/vue'
 
 import TipeIcon from '../../src/components/Icon.vue'
 
-storiesOf('Icon', module).add('default', () => ({
-  components: { TipeIcon },
-  computed: {
-    style() {
-      return {
-        height: '1rem',
-        width: '1rem'
-      }
-    }
-  },
-  template: '<tipe-icon :style="style" />'
-}))
+const style = () => ({
+  height: '1rem',
+  width: '1rem'
+})
+
+storiesOf('Icon', module)
+  .add('file', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="file" />'
+  }))
+  .add('files', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="files" />'
+  }))
+  .add('folder', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="folder" />'
+  }))
+  .add('image', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="image" />'
+  }))
+  .add('layout', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="layout" />'
+  }))
+  .add('plus', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="plus" />'
+  }))
+  .add('settings', () => ({
+    components: { TipeIcon },
+    computed: { style },
+    template: '<tipe-icon :style="style" icon="settings" />'
+  }))
