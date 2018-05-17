32901c86a9d98f117abf9d9f6efc60bb54d3a206
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-16 21:15:28 -0700

    Layout card component: in progress layout card

---
+++ src/components/LayoutCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div 
+    :data-tipe-ui="$options.name" 
+    class="card">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutCard'
+}
+</script>
+
+
+<style lang='postcss' scoped>
+.card {
+  border: solid 1px var(--gray-blue-light);
+  border-radius: 5px;
+  background-color: #fff;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  padding: 15px;
+  margin: 10px;
+}
+</style>
--- stories/card.stories.js
+++ stories/card.stories.js
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/vue'
 
 import FolderCard from '../src/components/FolderCard.vue'
 import DocCard from '../src/components/DocCard.vue'
+import LayoutCard from '../src/components/LayoutCard.vue'
 
 storiesOf('Cards', module)
   .add('Folder Card', () => ({
@@ -15,4 +16,8 @@ storiesOf('Cards', module)
   .add('Doc Card recent', () => ({
     components: { DocCard },
     template: '<doc-card recent>About</doc-card>'
+  .add('Layout Card', () => ({
+    components: { LayoutCard, FolderCard },
+    template:
+      '<layout-card><folder-card>Hero Sections</folder-card></layout-card>'
   }))
