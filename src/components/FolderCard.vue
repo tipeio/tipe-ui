97405f3fc542854a093fe3305de6fca5d0441c00
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-16 17:06:55 -0700

    Folder Card component: add hover but style is temp

--- src/components/FolderCard.vue
+++ src/components/FolderCard.vue
@@ -28,6 +28,10 @@ export default {
   justify-content: space-between;
   align-items: center;
 
+  &:hover {
+    background-color: black;
+  }
+
   & .content {
     display: flex;
     align-items: center;
