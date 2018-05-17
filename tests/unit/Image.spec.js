20783ba7f0a794697fc005d69c36468c62f64622
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-17 09:46:44 -0700

    image tests

---
+++ tests/unit/Image.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount } from '@vue/test-utils'
+import Image from '@/components/Image.vue'
+import faker from 'faker'
+
+describe('Image', () => {
+  it('matched previous snapshot', () => {
+    const wrapper = shallowMount(Image)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('has correct data-tipe-iu attibute', () => {
+    const wrapper = shallowMount(Image)
+
+    expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeImage')
+  })
+
+  describe('url is not provided', () => {
+    it('shows error state', () => {
+      const wrapper = shallowMount(Image)
+
+      expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeImage')
+    })
+  })
+
+  describe('alt is not provided', () => {
+    it('img alt attribute should be empty', () => {
+      const wrapper = shallowMount(Image)
+
+      expect(wrapper.find('img').attributes().alt).toBe('')
+    })
+  })
+
+  describe('alt is provided', () => {
+    it('img alt attribute should match alt prop', () => {
+      const propsData = { alt: faker.lorem.word() }
+      const wrapper = shallowMount(Image, { propsData })
+
+      expect(wrapper.find('img').attributes().alt).toBe(propsData.alt)
+    })
+  })
+
+  describe('url is provided', () => {
+    it('img src attribute should match url prop', () => {
+      const propsData = { url: faker.internet.url() }
+      const wrapper = shallowMount(Image, { propsData })
+
+      expect(wrapper.find('img').attributes().src).toBe(propsData.url)
+    })
+  })
+})
