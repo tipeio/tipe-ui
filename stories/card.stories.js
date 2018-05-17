71dc5ba7d9e58463a5eb3436e5e01afbab4a4082
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-16 17:02:10 -0700

    Folder Card component: add tests for folder card

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/vue'
 
 import FolderCard from '../src/components/FolderCard.vue'
 
-storiesOf('Card', module).add('Folder Card', () => ({
+storiesOf('Cards', module).add('Folder Card', () => ({
   components: { FolderCard },
   template: '<folder-card>Hero Sections</folder-card>'
 }))
