9c5860430155a3364219ac1d33de2175ec597605
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-16 17:43:42 -0700

    Doc Card component: add doc card component in progress

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -1,8 +1,18 @@
 import { storiesOf } from '@storybook/vue'
 
 import FolderCard from '../src/components/FolderCard.vue'
+import DocCard from '../src/components/DocCard.vue'
 
-storiesOf('Cards', module).add('Folder Card', () => ({
-  components: { FolderCard },
-  template: '<folder-card>Hero Sections</folder-card>'
-}))
+storiesOf('Cards', module)
+  .add('Folder Card', () => ({
+    components: { FolderCard },
+    template: '<folder-card>Hero Sections</folder-card>'
+  }))
+  .add('Doc Card default', () => ({
+    components: { DocCard },
+    template: '<doc-card>About</doc-card>'
+  }))
+  .add('Doc Card recent', () => ({
+    components: { DocCard },
+    template: '<doc-card recent>About</doc-card>'
+  }))
