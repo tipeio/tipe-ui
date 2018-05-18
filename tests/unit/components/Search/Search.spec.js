2a22fc67c88096cbfe2d9ba5e79301046393d8e3
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-18 14:42:03 -0700

    refactored search test

--- tests/unit/components/Search/Search.spec.js
+++ tests/unit/components/Search/Search.spec.js
@@ -1,36 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import Search from '@/components/Search'
-import Icon from '@/components/Icon'
 
 describe('Search', () => {
-  it('has correct data-tipe-iu attibute', () => {
-    const wrapper = shallowMount(Search)
-    expect(wrapper).toMatchSnapshot()
+  describe('<template>', () => {
+    it('matches previous snapshot', () => {
+      const wrapper = shallowMount(Search)
 
-    expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeSearch')
-  })
+      expect(wrapper).toMatchSnapshot()
+    })
 
-  it('has input with type search', () => {
-    const wrapper = shallowMount(Search)
-    expect(wrapper).toMatchSnapshot()
+    it('has correct data-tipe-iu attibute', () => {
+      const wrapper = shallowMount(Search)
 
-    expect(wrapper.find('input[type="search"]').exists()).toBe(true)
-  })
-
-  it('has correct placeholder', () => {
-    const wrapper = shallowMount(Search)
-    expect(wrapper).toMatchSnapshot()
-
-    expect(
-      wrapper.find('input[placeholder="Find everthing..."]').exists()
-    ).toBe(true)
-  })
-
-  it('has a search icon', () => {
-    const wrapper = shallowMount(Search)
-    expect(wrapper).toMatchSnapshot()
-
-    expect(wrapper.find(Icon).exists()).toBe(true)
-    expect(wrapper.find(Icon).props().icon).toBe('search')
+      expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeSearch')
+    })
   })
 })
