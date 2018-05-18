5653c49aba767aef2b1c3e1aeb4e992b3690a614
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-18 08:58:05 -0700

    fix, image story import correct image path

--- stories/Image.stories.js
+++ stories/Image.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/vue'
 
-import TipeImage from '../src/components/Image/'
+import TipeImage from '../src/components/Image'
 
 const style = {
   height: '100px',
