e335178e9b9b8c188d1f6a09965217c4846d5b30
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-18 11:44:53 -0700

    Stories: fix story descriptions to match garys

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -6,72 +6,72 @@ import TipeButton from '../src/components/Button'
 import '../src/styles/globals.css'
 
 storiesOf('Button', module)
-  .add('Fill/Default Button', () => ({
+  .add('default button', () => ({
     components: { TipeButton },
     template: '<tipe-button @click="action">Fill Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Outline Button', () => ({
+  .add('outline button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button outline @click="action">Outline Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Small Button', () => ({
+  .add('small button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button size="small" @click="action">Small Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Medium Button', () => ({
+  .add('medium button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button size="medium" @click="action">Medium Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Large Button', () => ({
+  .add('large button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button size="large" @click="action">Large Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Danger Button', () => ({
+  .add('danger button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="danger" @click="action">Danger Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Dark Button', () => ({
+  .add('dark purple button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="dark-purple" dark @click="action">Dark Button</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Danger Outline Button', () => ({
+  .add('danger outline button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="danger" outline @click="action">Danger Outline</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Dark Outline Button', () => ({
+  .add('dark purple outline button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="dark-purple" outline @click="action">Dark Outline</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Small Outline Danger Button', () => ({
+  .add('small outline danger button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="danger" size="small" outline @click="action">Small Outline Danger</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Large Outline Grey Button', () => ({
+  .add('large outline grey button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="gray" size="large" outline @click="action">Large Outline Gray</tipe-button>',
     methods: { action: action('clicked') }
   }))
-  .add('Icon Button', () => ({
+  .add('icon button', () => ({
     components: { TipeButton },
     template:
       '<tipe-button color="purple" size="small" icon="plus" @click="action">Icon Button</tipe-button>',
