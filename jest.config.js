79d1b9821ad054f2717ed4cd1b2ee483c9243313
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-18 12:57:01 -0700

    jest.config mocks file imports

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,10 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/unit/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/tests/unit/__mocks__/styleMock.js'
   },
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: [
