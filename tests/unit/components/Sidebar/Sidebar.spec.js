32616dbe297e2e470ab9cecfe7aaac7d841c1e90
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-21 16:19:22 -0700

    sidebar

---
+++ tests/unit/components/Sidebar/Sidebar.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils'
+import Sidebar from '@/components/Sidebar'
+import SidebarLink from '@/components/Sidebar/SidebarLink.vue'
+import faker from 'faker'
+
+const createLink = () => ({
+  icon: faker.lorem.word(),
+  label: faker.lorem.word(),
+  url: faker.internet.url(),
+  active: faker.random.boolean()
+})
+
+describe('Sidebar', () => {
+  describe('<template>', () => {
+    it('matches previous snapshot', () => {
+      const wrapper = shallowMount(Sidebar)
+
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('has correct data-tipe-iu attibute', () => {
+      const wrapper = shallowMount(Sidebar)
+
+      expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeSidebar')
+    })
+  })
+
+  describe(':props', () => {
+    it(':links - should render links', () => {
+      const propsData = { links: [createLink(), createLink()] }
+      const wrapper = shallowMount(Sidebar, { propsData })
+      const links = wrapper.findAll(SidebarLink)
+
+      expect(links.at(0).props()).toEqual(
+        expect.objectContaining(propsData.links[0])
+      )
+
+      expect(links.at(1).props()).toEqual(
+        expect.objectContaining(propsData.links[1])
+      )
+    })
+  })
+})
---
+++ tests/unit/components/Sidebar/SidebarLink.spec.js
@@ -0,0 +1,74 @@
+import { shallowMount } from '@vue/test-utils'
+import SidebarLink from '@/components/Sidebar/SidebarLink.vue'
+import Icon from '@/components/Icon'
+import faker from 'faker'
+
+describe('Sidebar', () => {
+  describe('<template>', () => {
+    it('matches previous snapshot', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('<root> - has correct data-tipe-iu attibute', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeSidebarLink')
+    })
+
+    it('<root> - should be a tag', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper.is('a')).toBe(true)
+    })
+
+    it('<tipe-icon> - should exist', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper.find(Icon).exists()).toBe(true)
+    })
+  })
+
+  describe(':props', () => {
+    it(':label - should match text', () => {
+      const propsData = { label: faker.lorem.words(1) }
+      const wrapper = shallowMount(SidebarLink, { propsData })
+
+      expect(wrapper.text()).toBe(propsData.label)
+    })
+
+    it(':url - should default to #', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper.attributes().href).toBe('#')
+    })
+
+    it(':url - should match href', () => {
+      const propsData = { url: faker.internet.url() }
+      const wrapper = shallowMount(SidebarLink, { propsData })
+
+      expect(wrapper.attributes().href).toBe(propsData.url)
+    })
+
+    it(':icon - should be passed to Icon component', () => {
+      const propsData = { icon: faker.lorem.word() }
+      const wrapper = shallowMount(SidebarLink, { propsData })
+
+      expect(wrapper.find(Icon).props().icon).toBe(propsData.icon)
+    })
+
+    it(':compressed(false) - should not add compressed class', () => {
+      const wrapper = shallowMount(SidebarLink)
+
+      expect(wrapper.classes()).not.toContain('compressed')
+    })
+
+    it(':compressed(true) - should add compressed class', () => {
+      const propsData = { compressed: true }
+      const wrapper = shallowMount(SidebarLink, { propsData })
+
+      expect(wrapper.classes()).toContain('compressed')
+    })
+  })
+})
