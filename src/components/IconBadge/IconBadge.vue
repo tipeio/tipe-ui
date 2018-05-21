5c610c18f7f86a80f8079a1a829be2935f057786
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-21 11:55:56 -0700

    Icon badge component: add icon badge component for account settings menu

---
+++ src/components/IconBadge/IconBadge.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <tipe-icon
+      :icon="icon"
+      class="icon"
+    />
+  </div>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import TipeIcon from '../Icon'
+
+export default {
+  name: 'IconBadge',
+  components: {
+    TipeIcon
+  },
+  props: {
+    icon: vueTypes.string
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+.card {
+  width: 3.4375rem;
+  height: 3.4375rem;
+  border-radius: 4px;
+  background-color: var(--purple-transparent);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & .icon {
+    opacity: 1;
+  }
+}
+</style>
