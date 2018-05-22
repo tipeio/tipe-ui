c34a4d26d2b8eb41c0ba489957bce2edfff55f91
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-21 17:44:04 -0700

    in progress accordian component

---
+++ src/components/Accordian/Accordian.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="content">
+    <div 
+      class="row" 
+      @click="onClick">
+      <p class="title" >{{ text.toUpperCase() }}</p>
+      <tipe-icon
+        icon="plus"
+        class="icon"
+      />
+    </div>
+    <slot v-if="open"/>
+  </div>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import TipeIcon from '../Icon'
+
+export default {
+  name: 'Accordian',
+  components: {
+    TipeIcon
+  },
+  props: {
+    text: vueTypes.string
+  },
+  data: function() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    onClick() {
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+.content {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  width: 199px;
+
+  & .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 2.125rem;
+    width: 100%;
+    border-bottom: 1px solid #eeeeee;
+    margin-bottom: 4px;
+
+    & .icon {
+      display: flex;
+      flex: 0 0 2rem;
+    }
+    & p {
+      margin: 0;
+      font-size: 0.6875rem;
+      color: #dce0e5;
+    }
+  }
+}
+</style>
---
+++ stories/Accordian/Accordian.stories.js
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/vue'
+
+import Accordian from '../../src/components/Accordian'
+import ActivityItem from '../../src/components/RecentActivity/ActivityItem'
+
+storiesOf('Accordian', module).add('accordian', () => ({
+  components: { Accordian, ActivityItem },
+  template:
+    '<accordian  text="Recent Activity"><activity-item title="Sam Smith update api key" time="a moment ago"/></accordian>'
+}))
