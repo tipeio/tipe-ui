1bb8e16b42c1403b3b07abd81937ccb471025947
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-22 17:36:31 -0700

    module resolution config

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/unit/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/tests/unit/__mocks__/styleMock.js'
+    '\\.(css|less|pcss)$': '<rootDir>/tests/unit/__mocks__/styleMock.js'
   },
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: [
