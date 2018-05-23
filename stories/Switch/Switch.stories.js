c61d301fad9a85a53e0ba29e389dcef4889b6295
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-22 17:39:10 -0700

    Add: in progress switch component

---
+++ stories/Switch/Switch.stories.js
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/vue'
+
+import TipeSwitch from '../../src/components/Switch'
+
+storiesOf('Tipe Switch', module).add('switch', () => ({
+  components: { TipeSwitch },
+  template: '<tipe-switch />'
+}))
