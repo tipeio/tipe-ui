07238833e317d4ae0610cc98e3b8c2603b0458c0
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-23 14:16:28 -0700

    add sub label to switch component

--- stories/Accordian/Accordian.stories.js
+++ stories/Accordian/Accordian.stories.js
@@ -4,14 +4,18 @@ import Accordian from '../../src/components/Accordian'
 import { ActivityItem } from '../../src/components/AccountSettings/RecentActivity'
 
 const item = () => ({ title: 'Sam Smith update API key', time: '1 hour ago' })
-const style = () => ({ display: 'grid', 'grid-row-gap': '1rem' })
+const style = () => ({
+  display: 'grid',
+  'grid-row-gap': '1rem',
+  width: '250px'
+})
 
 storiesOf('Accordian', module)
   .add('accordian', () => ({
     components: { Accordian, ActivityItem },
-    computed: { item },
+    computed: { item, style },
     template:
-      '<accordian text="Recent Activity"><activity-item :item="item"/></accordian>'
+      '<div :style="style"><accordian text="Recent Activity"><activity-item :item="item"/></accordian></div>'
   }))
   .add('two accordians', () => ({
     components: { Accordian, ActivityItem },
