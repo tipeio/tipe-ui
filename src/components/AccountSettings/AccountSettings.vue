52ec1b6871887ed8ebe5b37a16320fad4fb48f0b
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-23 10:09:50 -0700

    add grid to account settings panel

--- src/components/AccountSettings/AccountSettings.vue
+++ src/components/AccountSettings/AccountSettings.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="content">
-    <div class="row">
-      <icon-badge icon="file" />
-      <space-progress 
-        :used-space="8.08" 
-        :total-space="40" 
-        unit="GB" />
-    </div>
-    <accordian text="Folder Details"/>
-    <accordian text="Settings"><tipe-switch text="File Sharing" /><tipe-switch text="Backup" /></accordian>
-    <accordian text="Recent Activity"><activity-item 
-      title="Sam Smith update api key" 
-      time="a moment ago"/><activity-item 
+  <div class="grid">
+    <div class="panel-content">
+      <div class="row">
+        <icon-badge icon="file" />
+        <space-progress 
+          :used-space="8.08" 
+          :total-space="40" 
+          unit="GB" />
+      </div>
+      <accordian text="Folder Details"/>
+      <accordian text="Settings"><tipe-switch text="File Sharing" /><tipe-switch text="Backup" /></accordian>
+      <accordian text="Recent Activity"><activity-item 
         title="Sam Smith update api key" 
-        time="a moment ago"/></accordian>
+        time="a moment ago"/><activity-item 
+          title="Sam Smith update api key" 
+          time="a moment ago"/></accordian>
+    </div>
   </div>
 </template>
 
@@ -37,9 +39,19 @@ export default {
 </script> 
 
 <style scoped lang="postcss">
-.content {
-  width: 252px;
+.grid {
   display: grid;
+  width: 252px;
+  grid-template-columns: 1rem auto 1rem;
+  grid-template-areas:
+    '. . .'
+    '. content .'
+    '. . .';
+}
+.panel-content {
+  display: grid;
+  grid-area: content;
+  grid-auto-flow: row;
   grid-row-gap: 1.5rem;
 }
 
