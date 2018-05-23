7c6192f56848c9ea5154d1c0675070f8d8bbf962
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-22 18:05:20 -0700

    swicth: clean up css in swiych component

--- src/components/Switch/Switch.vue
+++ src/components/Switch/Switch.vue
@@ -16,13 +16,13 @@ export default {
 .switch {
   position: relative;
   display: inline-block;
-  width: 60px;
+  width: 56px;
   height: 24px;
-}
+  /* Hide default HTML checkbox */
 
-/* Hide default HTML checkbox */
-.switch input {
-  display: none;
+  input {
+    display: none;
+  }
 }
 
 /* The slider */
@@ -30,47 +30,55 @@ export default {
   position: absolute;
   cursor: pointer;
   top: 0;
-  left: 0;
   right: 0;
+  left: 0;
   bottom: 0;
   -webkit-transition: 0.4s;
   transition: 0.4s;
   background-color: rgba(152, 152, 152, 0.11);
+
+  &.round {
+    border-radius: 24px;
+    &:before {
+      border-radius: 50%;
+    }
+  }
+
+  &:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 0px;
+    bottom: -3px;
+    background-color: white;
+    border: 2px #d0d7dd solid;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 }
 
-.slider:before {
-  position: absolute;
-  content: '';
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: -1.5px;
-  background-color: white;
-  border: 2px #d0d7dd solid;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
+input:checked {
+  & + .slider:before {
+    left: 1px;
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 
-input:checked + .slider {
-  background-image: var(--purple-gradient);
+  & + .slider {
+    background-image: var(--purple-gradient);
+    left: 0;
+    width: 56px !important;
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
 }
 
 input:focus + .slider {
-  box-shadow: 0 0 1px #2196f3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 24px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.1);
 }
 </style>
