a54792446faab9a8572143392af5b2575535508b
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-23 22:14:10 -0700

    storybook webpack config for postcss and svg

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,23 +1,33 @@
 const path = require('path')
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  const cssRule = defaultConfig.module.rules.find(
+    r => r.test.toString() === /\.css$/.toString()
+  )
+  const postCssUse = cssRule.use.find(
+    u => u.loader && u.loader.indexOf('postcss-loader') > -1
+  )
+  delete postCssUse.options
+
+  defaultConfig.resolve.alias['@'] = path.join(__dirname, '..', 'src')
+
+  const fileRule = defaultConfig.module.rules.find(r =>
+    /svg/.test(r.test.toString())
+  )
+
+  defaultConfig.module.rules = defaultConfig.module.rules.filter(
+    r => !/svg/.test(r.test.toString())
+  )
+
   defaultConfig.module.rules.push({
-    test: /\.css$/,
-    use: [
-      { loader: 'style-loader' },
-      { loader: 'css-loader', options: { importLoaders: 1 } },
-      { loader: 'postcss-loader' }
-    ],
-    include: path.join(__dirname, '/src')
+    ...fileRule,
+    test: new RegExp(fileRule.test.toString().replace(/\|svg/, ''))
   })
 
-  defaultConfig.resolve = {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      '@': path.join(__dirname, '..', 'src')
-    }
-  }
+  defaultConfig.module.rules.push({
+    test: /\.svg$/,
+    loader: 'svg-sprite-loader'
+  })
 
   return defaultConfig
 }
