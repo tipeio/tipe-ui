606905cf7f9e2819961c11f4ead2af6a35ffa768
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-24 16:26:29 -0700

    Accordian: added tests for accordian component

---
+++ tests/unit/components/Accordian/Accordian.spec.js
@@ -0,0 +1,62 @@
+import { shallowMount } from '@vue/test-utils'
+import Accordian from '@/components/Accordian'
+import Icon from '@/components/Icon'
+
+describe('Accordian.vue', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: {
+        text: 'My Stuff'
+      }
+    })
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should have the content classs', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: {
+        text: 'My Stuff'
+      }
+    })
+    expect(wrapper.classes()).toContain('content')
+  })
+  it('should have the correct uppercased title', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: {
+        text: 'My Stuff'
+      }
+    })
+    expect(wrapper.find('.title').text()).toBe('MY STUFF')
+  })
+  it('should render the icon component', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: {
+        text: 'My Stuff'
+      }
+    })
+    expect(wrapper.contains(Icon)).toBe(true)
+  })
+  it('should not render the slotOpen class by default', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: {
+        text: 'My Stuff'
+      }
+    })
+    expect(wrapper.find('.slotOpen').exists()).toBe(false)
+  })
+  it('should show slot onClick', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: { text: 'Settings' }
+    })
+    wrapper.trigger('click')
+    expect(wrapper.find('.slotOpen').exists()).toBe(true)
+  })
+  it('should toggle the slot', () => {
+    const wrapper = shallowMount(Accordian, {
+      propsData: { text: 'Settings' }
+    })
+    wrapper.trigger('click')
+    expect(wrapper.find('.slotOpen').exists()).toBe(true)
+    wrapper.trigger('click')
+    expect(wrapper.find('.slotOpen').exists()).toBe(false)
+  })
+})
