d2b3f3c59b3eae0d52c17b17055d48eca97a9392
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-24 15:01:57 -0700

    Icon badge: added better tests

--- tests/unit/components/IconBadge/IconBadge.spec.js
+++ tests/unit/components/IconBadge/IconBadge.spec.js
@@ -1,9 +1,38 @@
 import { shallowMount } from '@vue/test-utils'
 import IconBadge from '@/components/IconBadge'
+import Icon from '@/components/Icon'
 
 describe('IconBadge.vue', () => {
   it('renders', () => {
-    const wrapper = shallowMount(IconBadge)
+    const wrapper = shallowMount(IconBadge, {
+      propsData: {
+        icon: 'rocket'
+      }
+    })
     expect(wrapper).toMatchSnapshot()
+  })
+  it('should render with the card class', () => {
+    const wrapper = shallowMount(IconBadge, {
+      propsData: {
+        icon: 'rocket'
+      }
+    })
+    expect(wrapper.classes()).toContain('card')
+  })
+  it('should pass the icon prop', () => {
+    const wrapper = shallowMount(IconBadge, {
+      propsData: {
+        icon: 'rocket'
+      }
+    })
+    expect(wrapper.props().icon).toBe('rocket')
+  })
+  it('should contain the icon component', () => {
+    const wrapper = shallowMount(IconBadge, {
+      propsData: {
+        icon: 'rocket'
+      }
+    })
+    expect(wrapper.find(Icon).exists()).toBe(true)
   })
 })
