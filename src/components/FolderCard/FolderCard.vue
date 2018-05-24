1566beed7f3717e292652172e36909853f1b9f73
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-24 11:27:54 -0700

    FolderCard

--- src/components/FolderCard/FolderCard.vue
+++ src/components/FolderCard/FolderCard.vue
@@ -1,50 +1,76 @@
 <template>
-  <div
-    :data-tipe-ui="$options.name"
-    class="card">
-    <div class="icon"><icon icon="folder"/></div>
-    <div class="label"><p><slot /></p></div>
-    <div class="icon"><icon icon="options"/></div>
-  </div>
+  <tipe-card :data-tipe-ui="$options.name">
+    <div :class="$style.layout">
+      <div :class="$style.header">
+        <div class="title">Folder</div>
+        <tipe-button
+          size="small"
+          icon-before="plus"
+        >
+          Add Folders
+        </tipe-button>
+      </div>
+      <div :class="$style.body">
+        <div :class="$style.scrollable">
+          <tipe-finder :files="files" />
+        </div>
+      </div>
+    </div>
+  </tipe-card>
 </template>
 
 <script>
-import Icon from '../Icon'
+import vueTypes from 'vue-types'
+import TipeCard from '@/components/LayoutCard'
+import TipeButton from '@/components/Button'
+import TipeFinder from '@/components/Finder'
+
 export default {
-  name: 'FolderCard',
+  name: 'TipeDocumentCard',
   components: {
-    Icon
+    TipeCard,
+    TipeButton,
+    TipeFinder
+  },
+  props: {
+    files: vueTypes.array
   }
 }
 </script>
 
+<style lang="postcss" module>
+.layout {
+  height: 100%;
+  width: 100%;
+  padding: 0 1.8rem 1.8rem 1.2rem;
+  display: grid;
+  grid-template-rows: 5rem 1fr;
+  box-sizing: border-box;
+}
 
-<style lang='postcss' scoped>
-.card {
-  border: solid 1px var(--gray-blue-light);
-  border-radius: 5px;
-  background-color: #fff;
-  height: 3.3125rem;
-  width: 263px;
-  display: flex;
-  justify-content: flex-start;
+.header {
+  display: grid;
+  grid-template-columns: 1fr auto;
   align-items: center;
+}
 
-  &:hover {
-    background-color: var(--gray-blue-light);
-  }
+.body {
+  overflow: hidden;
+}
 
-  & .icon {
-    display: flex;
-    flex: 0 0 3rem;
-    justify-content: center;
-  }
+/* hide scrollbar */
+.scrollable {
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  padding-right: 17px;
+  box-sizing: content-box;
+}
 
-  & .label {
-    display: flex;
-    flex: 1 1 auto;
-    color: #323c47;
-    font-size: 0.8125rem;
-  }
+.title {
+  font-size: 0.75rem;
+  color: var(--text-gray);
+  letter-spacing: 0.00625rem;
+  text-transform: uppercase;
 }
 </style>
