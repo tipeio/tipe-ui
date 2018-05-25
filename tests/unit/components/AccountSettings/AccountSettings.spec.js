61d1d09144217bdf20f2f3d746bc3e87004fa6dc
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-24 17:47:03 -0700

    Account settings: add account settings menu tests

---
+++ tests/unit/components/AccountSettings/AccountSettings.spec.js
@@ -0,0 +1,33 @@
+import { shallowMount } from '@vue/test-utils'
+import AccountSettings from '@/components/AccountSettings'
+import RecentActivity from '@/components/AccountSettings/RecentActivity'
+import Settings from '@/components/AccountSettings/Settings'
+import IconBadge from '@/components/IconBadge'
+import Progress from '@/components/Progress'
+
+describe('Settings.vue', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should have the pannel content class', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper.classes().contains('panel-content')).toBe(true)
+  })
+  it('should render the icon badge component', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper.contains(IconBadge)).toBe(true)
+  })
+  it('should render the progress component', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper.contains(Progress)).toBe(true)
+  })
+  it('should render the recent activity component', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper.contains(RecentActivity)).toBe(true)
+  })
+  it('should render the settings component', () => {
+    const wrapper = shallowMount(AccountSettings)
+    expect(wrapper.contains(Settings)).toBe(true)
+  })
+})
