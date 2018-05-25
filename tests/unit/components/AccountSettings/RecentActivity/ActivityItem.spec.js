91eae68fa6817e71e7f4e732c445cfa05adbf43a
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-24 17:05:59 -0700

    Activity item: add tests for activity item component

--- tests/unit/components/AccountSettings/RecentActivity/ActivityItem.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import ActivityItem from '@/components/RecentActivity/ActivityItem.vue'
-
-describe('ActivityItem.vue', () => {
-  it('renders', () => {
-    const wrapper = shallowMount(ActivityItem)
-    expect(wrapper).toMatchSnapshot()
-  })
-})
