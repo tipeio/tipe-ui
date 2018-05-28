1f79d3c04856c227cea339ddd54a923c635c7845
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-05-28 14:48:33 -0700

    ContentBrowser

--- stories/ContentBrowser/ContentBrowser.stories.js
+++ stories/ContentBrowser/ContentBrowser.stories.js
@@ -1,18 +1,41 @@
 import { storiesOf } from '@storybook/vue'
+import linkMock from '@/mocks/Link'
+import fileMock from '@/mocks/File'
 import TipeContentBrowser from '@/components/ContentBrowser'
 
 storiesOf('ContentBrowser', module).add('default', () => ({
   components: { TipeContentBrowser },
-  // data() {
-  //   return {
-  //     links: [createLink(), createLink(), createLink()]
-  //   }
-  // },
   computed: {
     styleObj: () => ({
       height: '500px',
       width: '500px'
-    })
+    }),
+    breadcrumbLinks: () => [linkMock(), linkMock(), linkMock()],
+    documents: () => [
+      fileMock({ type: 'document' }),
+      fileMock({ type: 'document' }),
+      fileMock({ type: 'document' }),
+      fileMock({ type: 'document' }),
+      fileMock({ type: 'document' }),
+      fileMock({ type: 'document' })
+    ],
+    folders: () => [
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' }),
+      fileMock({ type: 'folder' })
+    ]
   },
-  template: '<div :style="styleObj"><tipe-content-browser /></div>'
+  template: `
+    <div :style="styleObj">
+      <tipe-content-browser
+        :breadcrumbLinks="breadcrumbLinks"
+        :documents="documents"
+        :folders="folders"
+      />
+    </div>`
 }))
