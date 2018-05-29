ec6cdd7fcf86219d317655f75e73205b0430230c
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-29 13:51:05 -0700

    fix account settings tests

--- tests/unit/components/AccountSettings/RecentActivity/RecentActivity.spec.js
+++ tests/unit/components/AccountSettings/RecentActivity/RecentActivity.spec.js
@@ -1,7 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import RecentActivity, {
-  ActivityItem
-} from '@/components/AccountSettings/RecentActivity'
+import RecentActivity from '@/components/AccountSettings/RecentActivity'
 import Accordian from '@/components/Accordian'
 
 describe('RecentActivity.vue', () => {
@@ -12,9 +10,5 @@ describe('RecentActivity.vue', () => {
   it('should render the accordian component', () => {
     const wrapper = shallowMount(RecentActivity)
     expect(wrapper.contains(Accordian)).toBe(true)
-  })
-  it('should render the activity item component', () => {
-    const wrapper = shallowMount(RecentActivity)
-    expect(wrapper.contains(ActivityItem)).toBe(true)
   })
 })
