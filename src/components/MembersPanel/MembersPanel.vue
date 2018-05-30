a60001b081b8e4a6130903f2e69d693381732010
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-30 12:52:13 -0700

    Members Panel: add members pane l component with table

---
+++ src/components/MembersPanel/MembersPanel.vue
@@ -0,0 +1,109 @@
+<template>
+  <layout-card>
+    <div class="grid">
+      <div class="flex-row">
+        <div class="flex-column">
+          <p class="text">Members</p>
+          <p class="small-text">Description text goes here</p>
+        </div>
+        <tipe-button icon-before="plus">Add Member</tipe-button>
+      </div>
+      <tipe-table :members="members" />
+    </div>
+  </layout-card>
+</template>
+
+<script>
+import { TipeTable } from '.'
+import LayoutCard from '../LayoutCard'
+import TipeButton from '../Button'
+
+export default {
+  name: 'MembersPanel',
+  components: { LayoutCard, TipeTable, TipeButton },
+  data() {
+    return {
+      members: [
+        {
+          dateJoined: '05/02/2018',
+          id: '12',
+          name: 'Ada Miller',
+          email: 'ada_miller@gmail.com',
+          status: 'Active',
+          role: 'Owner',
+          url: 'http://placekitten.com/200/300'
+        },
+        {
+          dateJoined: '05/02/2018',
+          id: '34',
+          name: 'Ada Miller',
+          email: 'ada_miller@gmail.com',
+          status: 'Active',
+          role: 'Owner',
+          url: 'http://placekitten.com/200/300'
+        },
+        {
+          dateJoined: '05/02/2018',
+          id: '56',
+          name: 'Ada Miller',
+          email: 'ada_miller@gmail.com',
+          status: 'Active',
+          role: 'Owner',
+          url: 'http://placekitten.com/200/300'
+        },
+        {
+          dateJoined: '05/02/2018',
+          id: '78',
+          name: 'Ada Miller',
+          email: 'ada_miller@gmail.com',
+          status: 'Active',
+          role: 'Owner',
+          url: 'http://placekitten.com/200/300'
+        },
+        {
+          dateJoined: '05/02/2018',
+          id: '90',
+          name: 'Ada Miller',
+          email: 'ada_miller@gmail.com',
+          status: 'Active',
+          role: 'Owner',
+          url: 'http://placekitten.com/200/300'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
+<style lang='postcss' scoped>
+.grid {
+  display: grid;
+  grid-template-rows: 4rem auto;
+  width: 100%;
+}
+.flex-row {
+  display: flex;
+  max-width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.125rem;
+
+  & .text {
+    color: #323c47;
+    font-size: 0.875rem;
+    margin: 0;
+    margin-bottom: 3px;
+  }
+
+  & .small-text {
+    color: #323c47;
+    font-size: 0.75rem;
+    margin: 0;
+  }
+}
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/MembersPanel/Table.vue
@@ -0,0 +1,60 @@
+<template>
+  <table class="table">
+    <seperator />
+    <tr class="table-heading">
+      <th class="start">NAME</th>
+      <th>ROLE</th>
+      <th>DATE JOINED</th>
+      <th>STATUS</th>
+      <th>ACTIONS</th>
+    </tr>
+    <seperator />
+    <table-row 
+      v-for="member in members" 
+      :key="member.id" 
+      :member="member"
+    />
+  </table>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import { TableRow } from '.'
+import Seperator from '../Seperator'
+
+export default {
+  name: 'Table',
+  components: { TableRow, Seperator },
+  props: {
+    members: vueTypes.array
+  }
+}
+</script>
+
+
+<style lang='postcss' scoped>
+.table {
+  width: 100%;
+  height: 100%;
+}
+.table-heading {
+  display: flex;
+  max-width: 100%;
+  justify-content: space-evenly;
+  height: 2.5rem;
+  padding: 0 1rem;
+
+  & th {
+    color: #323c47;
+    font-size: 0.75rem;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+
+    &.start {
+      justify-content: flex-start;
+    }
+  }
+}
+</style>
