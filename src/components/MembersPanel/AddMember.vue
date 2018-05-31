c483ea2ca4fa5102730454860359c836ea67004c
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-31 12:00:05 -0700

    Member panel: add add member omodal..in progress

---
+++ src/components/MembersPanel/AddMember.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="grid"/>
+</template>
+
+<script>
+import { TipeTable } from '.'
+import { createManyMocks, user } from '../../mocks'
+import LayoutCard from '../LayoutCard'
+import TipeButton from '../Button'
+
+export default {
+  name: 'AddMember',
+  components: { LayoutCard, TipeTable, TipeButton },
+  data() {
+    return {
+      members: createManyMocks(user, 12),
+      modalOpen: false
+    }
+  },
+  methods: {
+    modal() {
+      this.modalOpen = !this.modalOpen
+    }
+  }
+}
+</script>
+
+
+<style lang='postcss' scoped>
+.grid {
+  display: grid;
+  background-color: #fff;
+  border-radius: 5px;
+  width: 500px;
+  height: 500px;
+}
+</style>
