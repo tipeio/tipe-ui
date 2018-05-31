f927250b7baf1fd7d2ba8f60c0ae7653acac4c47
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-05-31 12:00:05 -0700

    Member panel: add add member omodal..in progress

--- src/components/MembersPanel/index.js
+++ src/components/MembersPanel/index.js
@@ -1,3 +1,4 @@
 export TableRow from './TableRow'
 export TipeTable from './Table'
 export { default } from './MembersPanel'
+export AddMember from './AddMember'
---
+++ stories/AddMember/AddMember.stories.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/vue'
+
+import { AddMember } from '../../src/components/MembersPanel'
+
+const style = () => ({
+  width: '100vw',
+  height: '100vh',
+  'background-color': 'rgba(0, 0, 0, 0.3)',
+  display: 'flex',
+  'align-items': 'center',
+  'justify-content': 'center'
+})
+
+storiesOf('Add Member', module).add('account settings panel', () => ({
+  components: { AddMember },
+  computed: { style },
+  template: '<div :style="style"><add-member/></div>'
+}))
