2d8669ee51bbfc5e5b9ec626f10747feb9329ccd
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-01 09:12:48 -0700

    NewFile

---
+++ src/components/NewFile/NewFile.vue
@@ -0,0 +1,39 @@
+<template>
+  <tipe-tabs :data-tipe-ui="$options.name">
+    <tipe-tabs-panel
+      label="Documents"
+      icon="files"
+    >
+      <tipe-new-document :templates="templates"/>
+    </tipe-tabs-panel>
+    <tipe-tabs-panel
+      label="Folders"
+      icon="folder"
+    >
+      <tipe-new-folder :folders="folders"/>
+    </tipe-tabs-panel>
+  </tipe-tabs>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import FileShape from '@/types/File'
+import TipeTabs from '@/components/Tabs'
+import TipeTabsPanel from '@/components/TabsPanel'
+import TipeNewDocument from './NewDocument.vue'
+import TipeNewFolder from './NewFolder.vue'
+
+export default {
+  name: 'TipeNewFile',
+  components: {
+    TipeTabs,
+    TipeTabsPanel,
+    TipeNewDocument,
+    TipeNewFolder
+  },
+  props: {
+    templates: vueTypes.arrayOf(vueTypes.shape(FileShape)),
+    folders: vueTypes.arrayOf(vueTypes.shape(FileShape))
+  }
+}
+</script>
---
+++ src/components/NewFile/NewFolder.vue
@@ -0,0 +1,46 @@
+<template>
+  <section :data-tipe-ui="$options.name">
+    <tipe-file-name-input
+      message="Press Enter to create a folder."
+    />
+    <section class="template-finder-section">
+      <header>RECENT FOLDERS</header>
+      <tipe-finder
+        :files="folders"
+        layout="grid"
+      />
+    </section>
+  </section>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import FileShape from '@/types/File'
+import TipeFileNameInput from './FileNameInput.vue'
+import TipeFinder from '@/components/Finder'
+
+export default {
+  name: 'TipeNewDocument',
+  components: {
+    TipeFinder,
+    TipeFileNameInput
+  },
+  props: {
+    folders: vueTypes.arrayOf(vueTypes.shape(FileShape))
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.template-finder-section {
+  padding-top: 3.625rem;
+}
+
+.template-finder-section > header {
+  color: var(--purple-dark);
+  font-size: 1rem;
+  font-weight: 600;
+  padding-bottom: 1.6875rem;
+  box-sizing: border-box;
+}
+</style>
