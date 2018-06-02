3d267906cfc07baa4ac2eb557fe65081b5f1bc63
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-06-01 17:14:38 -0700

    Members panel: fix slow/broken members panel test spec

--- stories/MembersPanel/MembersPanel.stories.js
+++ stories/MembersPanel/MembersPanel.stories.js
@@ -1,13 +1,16 @@
 import { storiesOf } from '@storybook/vue'
-
+import { createManyMocks, user } from '../../src/mocks'
 import MembersPanel from '../../src/components/MembersPanel'
 
 const style = () => ({
   width: '1055px',
   height: '801px'
 })
+
+const members = () => createManyMocks(user, 12)
+
 storiesOf('Members Panel', module).add('members panel', () => ({
   components: { MembersPanel },
-  computed: { style },
-  template: '<div :style="style"><members-panel /></div>'
+  computed: { style, members },
+  template: '<div :style="style"><members-panel :members="members" /></div>'
 }))
--- tests/unit/components/MembersPanel/MembersPanel.spec.js
+++ tests/unit/components/MembersPanel/MembersPanel.spec.js
@@ -5,20 +5,89 @@ import LayoutCard from '@/components/LayoutCard'
 
 describe('MembersPanel.vue', () => {
   it('renders', () => {
-    const wrapper = shallowMount(MembersPanel)
+    const wrapper = shallowMount(MembersPanel, {
+      propsData: {
+        members
+      }
+    })
     expect(wrapper).toMatchSnapshot()
   })
   it('should render the LayoutCard component', () => {
-    const wrapper = shallowMount(MembersPanel)
+    const wrapper = shallowMount(MembersPanel, {
+      propsData: {
+        members
+      }
+    })
     expect(wrapper.contains(LayoutCard)).toBe(true)
   })
   it('should emit the modal method', () => {
-    const wrapper = shallowMount(MembersPanel)
+    const wrapper = shallowMount(MembersPanel, {
+      propsData: {
+        members
+      }
+    })
     wrapper.vm.$emit('modal')
     expect(wrapper.emitted().modal).toBeTruthy()
   })
   it('should have modalOpen set to false', () => {
-    const wrapper = shallowMount(MembersPanel)
+    const wrapper = shallowMount(MembersPanel, {
+      propsData: {
+        members
+      }
+    })
     expect(wrapper.vm.modalOpen).toBe(false)
   })
 })
+
+const members = [
+  {
+    id: '84f1a8ca-980d-4166-a227-35c94877a',
+    profileImageUrl: 'http://placekitten.com/g/200/300',
+    firstName: 'Garnett',
+    lastName: 'Koch',
+    email: 'Hank_Zemlak@hotmail.com',
+    role: { name: 'Owner' },
+    status: 'Offline',
+    updatedAt: 'Thu May 31 2018 22:44:03 GMT-0700 (Pacific Daylight Time)'
+  },
+  {
+    id: '84f1a8ca-0d-4166-a227-35c945f0877a',
+    profileImageUrl: 'http://placekitten.com/g/200/300',
+    firstName: 'Garnett',
+    lastName: 'Koch',
+    email: 'Hank_Zemlak@hotmail.com',
+    role: { name: 'Owner' },
+    status: 'Offline',
+    updatedAt: 'Thu May 31 2018 22:44:03 GMT-0700 (Pacific Daylight Time)'
+  },
+  {
+    id: '84f1a8ca-980d-4166-35c945f0877a',
+    profileImageUrl: 'http://placekitten.com/g/200/300',
+    firstName: 'Garnett',
+    lastName: 'Koch',
+    email: 'Hank_Zemlak@hotmail.com',
+    role: { name: 'Owner' },
+    status: 'Offline',
+    updatedAt: 'Thu May 31 2018 22:44:03 GMT-0700 (Pacific Daylight Time)'
+  },
+  {
+    id: '84f1a8ca-980d-4166-a227-35c9',
+    profileImageUrl: 'http://placekitten.com/g/200/300',
+    firstName: 'Garnett',
+    lastName: 'Koch',
+    email: 'Hank_Zemlak@hotmail.com',
+    role: { name: 'Owner' },
+    status: 'Offline',
+    updatedAt: 'Thu May 31 2018 22:44:03 GMT-0700 (Pacific Daylight Time)'
+  },
+  {
+    id: '980d-4166-a227-35c945f0877a',
+    profileImageUrl: 'http://placekitten.com/g/200/300',
+    firstName: 'Garnett',
+    lastName: 'Koch',
+    email: 'Hank_Zemlak@hotmail.com',
+    role: { name: 'Owner' },
+    status: 'Offline',
+    updatedAt: 'Thu May 31 2018 22:44:03 GMT-0700 (Pacific Daylight Time)'
+  }
+]
