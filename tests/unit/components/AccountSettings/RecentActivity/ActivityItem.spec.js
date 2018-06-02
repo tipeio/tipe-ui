4e44ce425025869262f1bcfc565cef9a962f16e2
Olivia Oddo <oliviaoddo@Olivias-MacBook-Pro.local>
2018-06-01 17:03:33 -0700

    Activity Item: remove extra test file causing missing item prop warning

--- tests/unit/components/AccountSettings/RecentActivity/ActivityItem.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import ActivityItem from '@/components/AccountSettings/RecentActivity/ActivityItem.vue'
-
-describe('ActivityItem.vue', () => {
-  it('renders', () => {
-    const wrapper = shallowMount(ActivityItem)
-    expect(wrapper).toMatchSnapshot()
-  })
-})
