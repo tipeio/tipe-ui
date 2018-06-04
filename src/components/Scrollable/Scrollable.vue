502fe9d85f1da1185a0555964fe449b09368bce7
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-04 14:04:38 -0700

    Scrollable

---
+++ src/components/Scrollable/Scrollable.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="$style.window">
+    <div :class="$style.scrollable">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" module>
+.window {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.scrollable {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  padding-right: 17px;
+  box-sizing: content-box;
+}
+</style>
