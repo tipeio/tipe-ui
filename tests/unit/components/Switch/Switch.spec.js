f24b9b46313b3d5c1e74df65407494d8e323a178
Olivia Oddo <olivia.oddo@gmail.com>
2018-06-08 11:02:41 -0700

    Code Review: fix tests for switch component

--- tests/unit/components/Switch/Switch.spec.js
+++ tests/unit/components/Switch/Switch.spec.js
@@ -3,7 +3,7 @@ import { createRenderer } from 'vue-server-renderer'
 import TipeSwitch from '@/components/Switch'
 
 const props = {
-  text: 'Share Location',
+  label: 'Share Location',
   name: 'location'
 }
 describe('Progress.vue', () => {
@@ -35,25 +35,6 @@ describe('Progress.vue', () => {
     })
     expect(wrapper.find('.label').text()).toBe('Share Location')
   })
-  it('should have the correct sublabel', () => {
-    const wrapper = shallowMount(TipeSwitch, {
-      propsData: {
-        ...props,
-        sublabel: 'Share location while using the app'
-      }
-    })
-    expect(wrapper.find('.sublabel').text()).toBe(
-      'Share location while using the app'
-    )
-  })
-  it('should not render with a sublabel', () => {
-    const wrapper = shallowMount(TipeSwitch, {
-      propsData: {
-        ...props
-      }
-    })
-    expect(wrapper.find('.sublabel').exists()).toBe(false)
-  })
   it('should have the correct value prop', () => {
     const wrapper = shallowMount(TipeSwitch, {
       propsData: {
@@ -67,13 +48,11 @@ describe('Progress.vue', () => {
     const wrapper = shallowMount(TipeSwitch, {
       propsData: {
         ...props,
-        sublabel: 'Share location while using the app',
         value: false,
         disabled: true
       }
     })
-    expect(wrapper.props().text).toBe('Share Location')
-    expect(wrapper.props().sublabel).toBe('Share location while using the app')
+    expect(wrapper.props().label).toBe('Share Location')
     expect(wrapper.props().value).toBe(false)
     expect(wrapper.props().name).toBe('location')
     expect(wrapper.props().disabled).toBe(true)
