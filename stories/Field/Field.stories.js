4a66131a3d318516a5728809a443b2f179dba612
Olivia Oddo <olivia.oddo@gmail.com>
2018-06-12 16:57:05 -0700

    Field: add more field stories

--- stories/Field/Field.stories.js
+++ stories/Field/Field.stories.js
@@ -3,15 +3,68 @@ import { storiesOf } from '@storybook/vue'
 import TipeField from '../../src/components/Field'
 import TipeTextInput from '../../src/components/TextInput'
 
-const field = () => ({
-  label: 'First Name',
-  placeholder: 'What is your first name?',
-  value: '',
-  size: 'normal'
+const style = () => ({
+  width: '200px'
 })
-storiesOf('Field', module).add('default input', () => ({
-  components: { TipeField, TipeTextInput },
-  computed: { field },
-  template:
-    '<tipe-field :label="field.label" name="firstName" :field="field"><tipe-text-input /></tipe-field>'
-}))
+
+storiesOf('Field', module)
+  .add('default input', () => ({
+    components: { TipeField, TipeTextInput },
+    computed: { style },
+    data() {
+      return {
+        field: {
+          label: 'First Name',
+          placeholder: 'What is your first name?',
+          value: ''
+        }
+      }
+    },
+    template:
+      '<div :style="style"><tipe-field :label="field.label" name="firstName" :field="field"><tipe-text-input /></tipe-field></div>'
+  }))
+  .add('error input', () => ({
+    components: { TipeField, TipeTextInput },
+    computed: { style },
+    data() {
+      return {
+        field: {
+          label: 'First Name',
+          placeholder: 'What is your first name?',
+          value: ''
+        }
+      }
+    },
+    template:
+      '<div :style="style"><tipe-field :label="field.label" name="firstName" :field="field" status="error" errorMessage="Invalid name"><tipe-text-input /></tipe-field></div>'
+  }))
+  .add('success input', () => ({
+    components: { TipeField, TipeTextInput },
+    computed: { style },
+    data() {
+      return {
+        field: {
+          label: 'First Name',
+          placeholder: 'What is your first name?',
+          value: 'Olivia'
+        }
+      }
+    },
+    template:
+      '<div :style="style"><tipe-field :label="field.label" name="firstName" :field="field" status="success"><tipe-text-input /></tipe-field></div>'
+  }))
+  .add('warning input', () => ({
+    components: { TipeField, TipeTextInput },
+    computed: { style },
+    data() {
+      return {
+        field: {
+          label: 'First Name',
+          placeholder: 'What is your first name?',
+          value: 'o'
+        }
+      }
+    },
+    template:
+      '<div :style="style"><tipe-field :label="field.label" name="firstName" :field="field" status="warning" warningMessage="Warning message watch out"><tipe-text-input /></tipe-field></div>'
+  }))
