c8e9fa48f6417cd2183ccc5c2c46770f5a3b9e94
Olivia Oddo <olivia.oddo@gmail.com>
2018-06-14 12:53:15 -0700

    Field: refactor field component to have a container sop the render function is smaller

---
+++ src/components/Field/FieldContainer.vue
@@ -0,0 +1,114 @@
+<template>
+  <div 
+    :data-tipe-ui="$options.name" 
+    :class="classObject" 
+    @click="click" 
+    @focus="focus" 
+    @change="change" 
+    @blur="blur">
+    <label 
+      v-if="label" 
+      :for="label">{{ label }}</label>
+    <tipe-field-input 
+      :field="field" 
+      :name="name" 
+      :status="status">
+      <slot />
+    </tipe-field-input>
+    <span 
+      v-if="message" 
+      class="message">{{ message }}</span>
+  </div>
+</template>
+
+<script>
+import TipeFieldInput from './FieldInput'
+import fieldShape from '@/types/Field'
+
+export default {
+  name: 'TipeFieldContainer',
+  components: { TipeFieldInput },
+  props: fieldShape,
+  computed: {
+    classObject: function() {
+      const { status } = this
+      return {
+        success: status === 'success',
+        error: status === 'error',
+        warning: status === 'warning'
+      }
+    },
+    message() {
+      switch (this.status) {
+        case 'error':
+          return this.errorMessage
+        case 'warning':
+          return this.warningMessage
+        case 'success':
+          return this.successMessage
+        default:
+          return ''
+      }
+    }
+  },
+  methods: {
+    click(event) {
+      this.$emit('click', event)
+    },
+    focus(event) {
+      this.$emit('focus', event)
+    },
+    blur(event) {
+      this.$emit('blur', event)
+    },
+    change(event) {
+      this.$emit('change', event)
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+div {
+  display: flex;
+  flex-direction: column;
+}
+label {
+  color: #627098;
+  font-weight: 500;
+  font-size: 0.8125rem;
+}
+
+.message {
+  font-size: 0.6875rem;
+  margin: 0;
+  margin-top: 6.5px;
+}
+.warning {
+  & label {
+    color: #f38438;
+  }
+
+  & .message {
+    color: #f38438;
+  }
+}
+.success {
+  & label {
+    color: #16e4a4;
+  }
+
+  & .message {
+    color: #16e4a4;
+  }
+}
+.error {
+  & label {
+    color: #e44646;
+  }
+
+  & .message {
+    color: #e44646;
+  }
+}
+</style>
--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,1 +1,1 @@
-export { default } from './Field.vue'
+export { default } from './FieldContainer.vue'
