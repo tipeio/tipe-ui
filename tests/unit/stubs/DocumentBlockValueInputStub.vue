bb77bcb3bfbd50956b4e66b9153733f63c66933c
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-14 15:23:18 -0700

    DocumentBlock: refactored

---
+++ tests/unit/stubs/DocumentBlockValueInputStub.vue
@@ -0,0 +1,16 @@
+<template>
+  <input
+    :value="value"
+    type="text"
+    @change="$emit('change', $event)"
+  >
+</template>
+
+<script>
+import documentBlockShape from '@/types/DocumentBlock'
+
+export default {
+  name: 'TipeDocumentBlockValueInputStub',
+  props: documentBlockShape
+}
+</script>
