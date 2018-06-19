07646664ea27f6aa9d4859dc3147f05e33258385
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-19 14:27:01 -0700

    DocumentBlockList

---
+++ src/components/DocumentEditor/DocumentBlockList/DocumentBlockList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :data-tipe-ui="$options.name">
+    <tipe-document-block
+      v-for="(block, i) in blocks"
+      :key="i"
+      v-bind="block"
+    />
+  </div>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import documentBlockShape from '@/types/DocumentBlock'
+import TipeDocumentBlock from '@/components/DocumentEditor/DocumentBlock'
+
+export default {
+  name: 'TipeDocumentBlockList',
+  components: {
+    TipeDocumentBlock
+  },
+  props: {
+    blocks: vueTypes.arrayOf(vueTypes.shape(documentBlockShape))
+  }
+}
+</script>
---
+++ tests/unit/components/DocumentEditor/DocumentBlockList.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from '@vue/test-utils'
+import { createRenderer } from 'vue-server-renderer'
+import DocumentBlock from '@/components/DocumentEditor/DocumentBlock'
+import DocumentBlockList from '@/components/DocumentEditor/DocumentBlockList'
+import documentBlockFixture from '@/fixtures/DocumentBlock'
+import documentBlockMock from '@/mocks/DocumentBlock'
+import createManyMocks from '@/mocks/createManyMocks'
+
+describe('DocumentBlockList', () => {
+  describe('<template>', () => {
+    it('matches previous snapshot', () => {
+      const renderer = createRenderer()
+      const propsData = documentBlockFixture()
+      const wrapper = shallowMount(DocumentBlockList, { propsData })
+
+      renderer.renderToString(wrapper.vm, (err, str) => {
+        if (err) throw new Error(err)
+        expect(str).toMatchSnapshot()
+      })
+    })
+
+    it('has correct data-tipe-iu attibute', () => {
+      const wrapper = shallowMount(DocumentBlockList)
+
+      expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeDocumentBlockList')
+    })
+  })
+
+  describe(':props', () => {
+    it(':blocks - should render a DocumentBlock components for each block', () => {
+      const propsData = { blocks: createManyMocks(documentBlockMock, 3) }
+      const wrapper = shallowMount(DocumentBlockList, { propsData })
+
+      expect(wrapper.findAll(DocumentBlock)).toHaveLength(
+        propsData.blocks.length
+      )
+    })
+
+    it(':blocks - should pass block to DocumentBlock component props', () => {
+      const propsData = { blocks: createManyMocks(documentBlockMock, 1) }
+      const wrapper = shallowMount(DocumentBlockList, { propsData })
+
+      expect(wrapper.find(DocumentBlock).props()).toEqual(propsData.blocks[0])
+    })
+  })
+
+  describe('@events', () => {})
+})
