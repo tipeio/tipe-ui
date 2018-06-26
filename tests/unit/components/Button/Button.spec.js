e1f032ad5626f4eaca6cc7fdae3ab2a07ac7463f
Olivia Oddo <olivia.oddo@gmail.com>
2018-06-26 15:02:52 -0700

    Button: add props test to button component

--- tests/unit/components/Button/Button.spec.js
+++ tests/unit/components/Button/Button.spec.js
@@ -7,6 +7,10 @@ describe('Button.vue', () => {
     const wrapper = shallowMount(Button)
     await expect(createRenderer().renderToString(wrapper.vm)).toMatchSnapshot()
   })
+  it('has correct data-tipe-iu attibute', () => {
+    const wrapper = shallowMount(Button)
+    expect(wrapper.attributes()['data-tipe-ui']).toBe('TipeButton')
+  })
 
   it('renders a defaut button', () => {
     const wrapper = shallowMount(Button)
@@ -44,7 +48,7 @@ describe('Button.vue', () => {
     expect(wrapper.classes()).toContain('large')
     expect(wrapper.classes()).not.toContain('outline')
   })
-  it('should render a purple button', () => {
+  it('should render a primary button', () => {
     const wrapper = shallowMount(Button, {
       propsData: { color: 'primary' }
     })
@@ -52,7 +56,7 @@ describe('Button.vue', () => {
     expect(wrapper.classes()).toContain('medium')
     expect(wrapper.classes()).not.toContain('outline')
   })
-  it('should render a small purple outline button', () => {
+  it('should render a small primary outline button', () => {
     const wrapper = shallowMount(Button, {
       propsData: { color: 'primary', outline: true, size: 'small' }
     })
@@ -68,5 +72,19 @@ describe('Button.vue', () => {
     wrapper.setMethods({ action: action('clicked') })
     wrapper.trigger('click')
     expect(action.mock.calls).toHaveLength(1)
+  })
+  it('should have the correct props', () => {
+    const wrapper = shallowMount(Button, {
+      propsData: {
+        color: 'danger',
+        iconAfter: 'plus',
+        iconBefore: 'arrow-down',
+        size: 'large'
+      }
+    })
+    expect(wrapper.props().color).toBe('danger')
+    expect(wrapper.props().iconAfter).toBe('plus')
+    expect(wrapper.props().iconBefore).toBe('arrow-down')
+    expect(wrapper.props().size).toBe('large')
   })
 })
