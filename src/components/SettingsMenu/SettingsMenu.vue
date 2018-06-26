a31151a5dde0b67c184fffe646bb3cbf697dc9c4
Olivia Oddo <olivia.oddo@gmail.com>
2018-06-26 09:54:53 -0700

    Settings menu: update settings style menu

--- src/components/SettingsMenu/SettingsMenu.vue
+++ src/components/SettingsMenu/SettingsMenu.vue
@@ -1,10 +1,10 @@
 <template>
   <ul>
-    <li><div class="icon"><icon icon="wrench"/></div><p class="label">General</p></li>
+    <li class="first"><div class="icon"><icon icon="wrench"/></div><p class="label">General</p></li>
     <li><div class="icon"><icon icon="users"/></div><p class="label">Members</p></li>
     <li><div class="icon"><icon icon="key"/></div><p class="label">API Key</p></li>
     <li><div class="icon"><icon icon="branch"/></div><p class="label">Webhooks</p></li>
-    <li><div class="icon"><icon icon="credit-card"/></div><p class="label">Billing</p></li>
+    <li class="last"><div class="icon"><icon icon="credit-card"/></div><p class="label">Billing</p></li>
   </ul>
 </template>
 
@@ -21,29 +21,38 @@ export default {
 
 <style lang="postcss">
 ul {
-  height: 305px;
+  height: 18.625rem;
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 191px;
+  width: 100%;
   justify-content: space-evenly;
   margin: 0;
   padding: 0;
+  border: 1px #e7e9ea solid;
+  border-radius: 4px;
 }
 li {
   display: flex;
   align-items: center;
-  height: 2.5625rem;
+  height: 3.625rem;
   list-style-type: none;
   justify-content: flex-start;
   width: 100%;
+  border-bottom: 1px solid #e7e9ea;
 
   &:hover {
     background-color: var(--purple);
-    border-radius: 4px;
 
     & .label {
       color: #fff;
+    }
+
+    &.first {
+      border-radius: 4px 4px 0 0;
+    }
+    &.last {
+      border-radius: 0 0 4px 4px;
     }
   }
 
@@ -60,6 +69,10 @@ li {
     font-size: 0.8125rem;
     margin: 0;
     text-align: center;
+  }
+
+  &.last {
+    border-bottom: none;
   }
 }
 </style>
--- stories/SettingsMenu/SettingsMenu.stories.js
+++ stories/SettingsMenu/SettingsMenu.stories.js
@@ -2,7 +2,11 @@ import { storiesOf } from '@storybook/vue'
 
 import SettingsMenu from '../../src/components/SettingsMenu'
 
+const style = () => ({
+  width: '191px'
+})
 storiesOf('Settings Menu', module).add('settings menu', () => ({
   components: { SettingsMenu },
-  template: '<settings-menu/>'
+  computed: { style },
+  template: '<div :style="style"><settings-menu/></div>'
 }))
