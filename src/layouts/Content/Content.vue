f860024d6e8224699fbd7293b91711a7362eb5b8
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-26 17:20:26 -0700

    ContentLayout

---
+++ src/layouts/Content/Content.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :data-tipe-ui="$options.name">
+    <div class="nav-container">
+      <tipe-breadcrumbs :links="breadcrumbLinks"/>
+    </div>
+    <div class="stage">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+import TipeBreadcrumbs from '@/components/Breadcrumbs'
+import LinkShape from '@/types/Link'
+
+export default {
+  name: 'TipeContentLayout',
+  components: {
+    TipeBreadcrumbs
+  },
+  props: {
+    breadcrumbLinks: vueTypes.arrayOf(vueTypes.shape(LinkShape))
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+[data-tipe-ui='TipeContentLayout'] {
+  display: grid;
+  grid-template-rows: 3.3125rem 1fr;
+  height: 100%;
+  width: 100%;
+}
+
+.nav-container {
+  display: flex;
+  align-content: center;
+  justify-items: flex-start;
+  background-color: #f7f8fb;
+  padding: 0 4.375rem;
+}
+
+.stage {
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1rem 3.75rem 0 3.75rem;
+}
+</style>
