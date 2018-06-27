0dd5e68f1ec79aedb600b8c8b2e462652933c136
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-06-26 17:19:38 -0700

    DocumentEditor: tabs and scrollable block list

--- src/components/DocumentEditor/DocumentEditor.vue
+++ src/components/DocumentEditor/DocumentEditor.vue
@@ -1,26 +1,59 @@
 <template>
   <div :data-tipe-ui="$options.name">
-    <tipe-document-block-list
-      :blocks="blocks"
-      :options="options"
-    />
+    <tipe-document-name-input :value="document.name"/>
+    <tipe-tabs>
+      <tipe-tabs-panel label="Page Content">
+        <tipe-scrollable>
+          <div class="block-list-container">
+            <tipe-document-block-list
+              :blocks="document.blocks"
+              :options="options"
+            />
+          </div>
+        </tipe-scrollable>
+      </tipe-tabs-panel>
+      <tipe-tabs-panel label="API">
+        <div>API</div>
+      </tipe-tabs-panel>
+    </tipe-tabs>
   </div>
 </template>
 
 <script>
 import vueTypes from 'vue-types'
 import blockOptionShape from '@/types/BlockOption'
-import documentBlockShape from '@/types/DocumentBlock'
+import documentShape from '@/types/Document'
 import TipeDocumentBlockList from './DocumentBlockList'
+import TipeDocumentNameInput from './DocumentNameInput'
+import TipeTabs from '@/components/Tabs'
+import TipeTabsPanel from '@/components/TabsPanel'
+import TipeScrollable from '@/components/Scrollable'
 
 export default {
   name: 'TipeDocumentEditor',
   components: {
-    TipeDocumentBlockList
+    TipeDocumentBlockList,
+    TipeDocumentNameInput,
+    TipeTabs,
+    TipeTabsPanel,
+    TipeScrollable
   },
   props: {
     options: vueTypes.arrayOf(vueTypes.shape(blockOptionShape)),
-    blocks: vueTypes.arrayOf(vueTypes.shape(documentBlockShape))
+    document: vueTypes.shape(documentShape)
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+[data-tipe-ui='TipeDocumentEditor'] {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+.block-list-container {
+  padding-top: 2rem;
+  padding-bottom: 10rem;
+}
+</style>
