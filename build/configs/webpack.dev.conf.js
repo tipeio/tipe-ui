6138298ae53940bd98539801a1cf28bc837d61d0
Gary <gary.thomas.ryan@gmail.com>
2018-07-31 16:38:01 -0700

    config: exclude @tipe deps in dev build

--- build/configs/webpack.dev.conf.js
+++ build/configs/webpack.dev.conf.js
@@ -2,5 +2,12 @@ const merge = require('webpack-merge')
 const base = require('./webpack.base.conf')
 
 module.exports = merge(base, {
+  externals: [
+    'vue',
+    'vue-types',
+    '@type/type-config',
+    '@tipe/tipe-interfaces',
+    '@tipe/tipe-constants'
+  ],
   devtool: 'cheap-module-eval-source-map'
 })
