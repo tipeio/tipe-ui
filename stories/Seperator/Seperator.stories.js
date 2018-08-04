972926ebdda796c88bdbec8e5443d39aa595d35e
Gary <gary.thomas.ryan@gmail.com>
2018-08-04 09:16:11 -0700

    Seperator: removed

--- stories/Seperator/Seperator.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue'
-
-import TipeSeperator from '../../src/components/Seperator'
-
-storiesOf('Seperator', module).add('default', () => ({
-  components: { TipeSeperator },
-  template: '<tipe-seperator>😀 😎 👍 💯</tipe-seperator>'
-}))
