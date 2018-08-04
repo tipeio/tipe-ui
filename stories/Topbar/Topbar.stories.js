cc21640cb8c57f78ecb4d9e4a25d4551e9ff5cc9
Gary <gary.thomas.ryan@gmail.com>
2018-08-04 10:01:01 -0700

    Topbar: remove

--- stories/Topbar/Topbar.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/vue'
-import TipeTopbar from '../../src/components/Topbar'
-
-storiesOf('Topbar', module).add('default', () => ({
-  components: { TipeTopbar },
-  template: '<tipe-topbar profileImageUrl="http://placekitten.com/200/200" />'
-}))
