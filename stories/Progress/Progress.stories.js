3e4b75c985d3e35402389001b3e86fb73b9b7d9d
Gary <gary.thomas.ryan@gmail.com>
2018-08-04 15:44:34 -0700

    Progress

--- stories/Progress/Progress.stories.js
+++ stories/Progress/Progress.stories.js
@@ -2,12 +2,7 @@ import { storiesOf } from '@storybook/vue'
 
 import TipeProgress from '../../src/components/Progress'
 
-storiesOf('Progress', module)
-  .add('space progress 20%', () => ({
-    components: { TipeProgress },
-    template: '<tipe-progress :used=8.08 :total=40 unit="GB" title="Space"/>'
-  }))
-  .add('completed progress 82%', () => ({
-    components: { TipeProgress },
-    template: '<tipe-progress :used=82 :total=100 title="Completed"/>'
-  }))
+storiesOf('Progress', module).add('default', () => ({
+  components: { TipeProgress },
+  template: '<tipe-progress value="65" />'
+}))
