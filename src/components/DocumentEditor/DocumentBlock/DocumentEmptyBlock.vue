32f26d250d232eb032c560a37d3b41b1d0ac6560
Gary <gary.thomas.ryan@gmail.com>
2018-08-04 19:30:27 -0700

    DocumentEditor: refactored to be simpler

--- src/components/DocumentEditor/DocumentBlock/DocumentEmptyBlock.vue
+++ src/components/DocumentEditor/DocumentBlock/DocumentEmptyBlock.vue
@@ -2,11 +2,6 @@
   <div
     :data-tipe-ui="$options.name"
   >
-    <div class="margin">
-      <tipe-document-block-margin
-        :options="options"
-      />
-    </div>
     <div class="body">
       Please add a block.
     </div>
@@ -16,13 +11,9 @@
 <script>
 import vueTypes from 'vue-types'
 import interfaces from '@tipe/tipe-interfaces'
-import TipeDocumentBlockMargin from './DocumentBlockMargin'
 
 export default {
   name: 'TipeDocumentEmptyBlock',
-  components: {
-    TipeDocumentBlockMargin
-  },
   props: {
     options: vueTypes.arrayOf(vueTypes.shape(interfaces.blockOption))
   }
@@ -31,10 +22,8 @@ export default {
 
 <style lang="postcss" scoped>
 [data-tipe-ui='TipeDocumentEmptyBlock'] {
-  display: grid;
-  grid-template-areas: 'margin body';
-  grid-template-columns: 4.25rem 1fr;
-  grid-template-rows: auto;
+  display: flex;
+  flex-direction: column;
   border-radius: 0.1875rem;
   box-sizing: border-box;
   width: 100%;
