cc0b6a20210c2b73bb86c1c519c87b3be5ad484d
Gary <gary.thomas.ryan@gmail.com>
2018-08-06 09:08:25 -0700

    Breadcrumbs: docs

--- stories/Breadcrumbs/Breadcrumbs.stories.js
+++ stories/Breadcrumbs/Breadcrumbs.stories.js
@@ -1,22 +1,39 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, object } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
+import { action } from '@storybook/addon-actions'
 import TipeBreadcrumbs from '../../src/components/Breadcrumbs'
 import faker from 'faker'
 
 const createLink = link => ({
   label: faker.lorem.word(),
+  name: faker.lorem.word(),
   ...link
 })
 
-storiesOf('Breadcrumbs', module).add('default', () => ({
-  components: { TipeBreadcrumbs },
-  data() {
-    return {
-      links: [
-        createLink({ icon: 'document' }),
-        createLink({ icon: 'folder' }),
-        createLink()
-      ]
-    }
-  },
-  template: '<tipe-breadcrumbs :links="links" />'
-}))
+const notes = `
+#### :props
+:links | { name: string, label: string, icon: string } |
+#### @events
+@click(link.name)
+`
+
+storiesOf('Breadcrumbs', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeBreadcrumbs },
+      methods: { click: action('click') },
+      data() {
+        return {
+          links: object('links', [
+            createLink({ icon: 'document' }),
+            createLink({ icon: 'folder' }),
+            createLink()
+          ])
+        }
+      },
+      template: '<tipe-breadcrumbs @click="click" :links="links" />'
+    }))
+  )
