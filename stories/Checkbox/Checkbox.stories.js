c3e30961cc01cb9c173299e57beb0c75f1ebb258
Gary <gary.thomas.ryan@gmail.com>
2018-08-06 09:58:05 -0700

    Checkbox: docs

--- stories/Checkbox/Checkbox.stories.js
+++ stories/Checkbox/Checkbox.stories.js
@@ -1,81 +1,72 @@
 import { storiesOf } from '@storybook/vue'
+import {
+  withKnobs,
+  select,
+  number,
+  boolean,
+  object
+} from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeCheckbox from '../../src/components/Checkbox'
+import { text } from '../../node_modules/@storybook/addon-knobs/dist/vue'
 
-const style = () => ({
-  width: '200px'
-})
+const notes = `
+#### :props
+:value | string or [string] |
+:name | string |
+:label | string |
+:size | string | mini, small, medium, large
+:waiting | boolean |
+:disabled | boolean |
+:tabIndex | number
+#### @events
+@click($event)
+@blur($event)
+@focus($event)
+@change($event)
+`
 
 storiesOf('Checkbox', module)
-  .add('default checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" />'
-  }))
-  .add('small checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" size="small" />'
-  }))
-  .add('large checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" size="large"/>'
-  }))
-  .add('success checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" status="success"/>'
-  }))
-  .add('warning checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" status="warning"/>'
-  }))
-  .add('error checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" status="error"/>'
-  }))
-  .add('waiting checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" :waiting="true"/>'
-  }))
-  .add('disabled checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" :disabled="true"/>'
-  }))
-  .add('checked checkbox', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template: '<tipe-checkbox label="JavaScript" :checked="true"/>'
-  }))
-  .add('name and value match', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    template:
-      '<tipe-checkbox label="JavaScript" name="JavaScript" value="JavaScript"/>'
-  }))
-  .add('name in value array', () => ({
-    components: { TipeCheckbox },
-    computed: { style },
-    data() {
-      return {
-        value: ['JavaScript', 'Python']
-      }
-    },
-    methods: {
-      onChange(value) {
-        this.value = value
-        console.log(value)
-      }
-    },
-    template: `
-      <div>
-        <tipe-checkbox label="JavaScript" name="JavaScript" :value="value" @change="onChange"/>
-        <tipe-checkbox label="Python" name="Python" :value="value" @change="onChange"/>
-      </div>
-      `
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeCheckbox },
+      data() {
+        return {
+          value: object('value', ['javascript']),
+          name: text('name', 'javascript'),
+          label: text('label', 'JavaScript'),
+          size: select('size', ['mini', 'small', 'medium', 'large'], 'medium'),
+          status: select('status', ['', 'success', 'warning', 'error'], ''),
+          waiting: boolean('waiting'),
+          disabled: boolean('disabled'),
+          tabIndex: number('tabIndex', 0)
+        }
+      },
+      methods: {
+        click: action('click'),
+        blur: action('blur'),
+        focus: action('focus'),
+        change: action('change')
+      },
+      template: `
+      <tipe-checkbox
+        @click="click"
+        @blur="blur"
+        @focus="focus"
+        @change="change"
+        :name="name"
+        :value="value"
+        :label="label"
+        :size="size"
+        :status="status"
+        :waiting="waiting"
+        :disabled="disabled"
+        :tabIndex="tabIndex"
+        :value="value"
+      />`
+    }))
+  )
