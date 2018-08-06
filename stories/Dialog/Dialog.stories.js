bf8cfafe42ee91aea9a4939bf51216a85e825195
Gary <gary.thomas.ryan@gmail.com>
2018-08-06 10:35:21 -0700

    Dialog: docs

--- stories/Dialog/Dialog.stories.js
+++ stories/Dialog/Dialog.stories.js
@@ -1,8 +1,41 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, text } from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeDialog from '../../src/components/Dialog'
 
-storiesOf('Dialog', module).add('default', () => ({
-  components: { TipeDialog },
-  template: '<tipe-dialog title="Hello World" message="Say hello back?" />'
-}))
+const notes = `
+#### :props
+:title | string |
+:message | string |
+#### @events
+@cancel()
+@continue()
+`
+
+storiesOf('Dialog', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeDialog },
+      data() {
+        return {
+          title: text('title', 'Hello World'),
+          message: text('message', 'Say hello back')
+        }
+      },
+      methods: {
+        cancel: action('cancel'),
+        ok: action('continue')
+      },
+      template: `
+      <tipe-dialog
+        @cancel="cancel"
+        @continue="ok"
+        :title="title"
+        :message="message"
+      />`
+    }))
+  )
