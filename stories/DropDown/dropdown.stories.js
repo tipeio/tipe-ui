7a4059f776f85ab905107bc1e598f3bb82683945
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-08 10:11:54 -0700

    DropDown: docs

--- stories/DropDown/dropdown.stories.js
+++ stories/DropDown/dropdown.stories.js
@@ -1,40 +1,46 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeDropDown from '../../src/components/DropDown'
 import TipeButton from '@/components/Button'
 
-const style = () => ({
-  width: '200px'
-})
+const notes = `
+#### :props
+:disabled | boolean |
+:waiting | boolean |
+:tabIndex | number |
+`
 
 storiesOf('DropDown', module)
-  .add('default', () => ({
-    components: { TipeDropDown, TipeButton },
-    computed: { style },
-    template: `
-      <tipe-drop-down name="firstName">
-        <div slot="control"><tipe-button size="small">Click Me!</tipe-button></div>
-        <div slot="panel">
-          <ul>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
-          </ul>
-        </div>
-      </tipe-drop-down>`
-  }))
-  .add('disabled', () => ({
-    components: { TipeDropDown, TipeButton },
-    computed: { style },
-    template: `
-      <tipe-drop-down name="firstName" disabled="true">
-        <div slot="control"><tipe-button size="small" disabled>Click Me!</tipe-button></div>
-        <div slot="panel">
-          <ul>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
-          </ul>
-        </div>
-      </tipe-drop-down>`
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeDropDown, TipeButton },
+      data() {
+        return {
+          disabled: boolean('disabled', false),
+          waiting: boolean('waiting', false),
+          tabIndex: text('tabIndex', 0)
+        }
+      },
+      template: `
+        <tipe-drop-down
+          :disabled="disabled"
+          :waiting="waiting"
+          :tabIndex="tabIndex"
+        >
+          <div slot="control">
+            <tipe-button size="small">Click Me!</tipe-button>
+          </div>
+          <div slot="panel">
+            <ul>
+              <li>option</li>
+              <li>option</li>
+              <li>option</li>
+            </ul>
+          </div>
+        </tipe-drop-down>`
+    }))
+  )
