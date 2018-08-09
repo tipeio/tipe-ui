df84517ee792d29eb79b0c5470b729bc27b225b9
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-09 16:30:02 -0700

    NumberInput: docs

--- stories/NumberInput/NumberInput.stories.js
+++ stories/NumberInput/NumberInput.stories.js
@@ -1,53 +1,66 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, select, number, boolean } from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeNumberInput from '@/components/NumberInput'
+import { text } from '../../node_modules/@storybook/addon-knobs/dist/vue'
+
+const notes = `
+#### :props
+:value | number |
+:select | string | mini, small, medium, large
+:min | number |
+:max | number |
+:step | number |
+
+#### @events
+@click($event)
+@focus($event)
+@blur($event)
+@change($event)
+`
 
 storiesOf('NumberInput', module)
-  .add('size(small)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" size="small" />'
-  }))
-  .add('size(medium)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" size="medium" />'
-  }))
-  .add('size(large)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" size="large" />'
-  }))
-  .add('status(success)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" status="success" />'
-  }))
-  .add('status(warning)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" status="warning" />'
-  }))
-  .add('status(error)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" status="error" />'
-  }))
-  .add('waiting', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" waiting />'
-  }))
-  .add('disabled', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input :value="3" disabled />'
-  }))
-  .add('placeholder', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input placeholder="value" />'
-  }))
-  .add('max(10)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input placeholder="value" :max="10" />'
-  }))
-  .add('min(0)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input placeholder="value" :min="0" />'
-  }))
-  .add('step(0.0001)', () => ({
-    components: { TipeNumberInput },
-    template: '<tipe-number-input placeholder="value" :step="0.0001" />'
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeNumberInput },
+      data() {
+        return {
+          value: number('value', 3),
+          placeholder: text('placeholder', 'Quantity'),
+          size: select('size', ['mini', 'small', 'medium', 'large'], 'medium'),
+          min: number('min', 0),
+          max: number('max', 100),
+          step: number('step', 1),
+          status: select('status', ['', 'success', 'warning', 'error'], ''),
+          disabled: boolean('disabled', false),
+          waiting: boolean('waiting', false)
+        }
+      },
+      methods: {
+        click: action('click'),
+        focus: action('focus'),
+        blur: action('blur'),
+        change: action('change')
+      },
+      template: `
+        <tipe-number-input
+          :value="value"
+          :placeholder="placeholder"
+          :size="size"
+          :min="min"
+          :max="max"
+          :step="step"
+          :status="status"
+          :disabled="disabled"
+          :waiting="waiting"
+          @click="click"
+          @focus="focus"
+          @blur="blur"
+          @change="change"
+        />`
+    }))
+  )
