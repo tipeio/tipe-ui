2d19ba9b48824cf4b1bea223156fc090acfa0a87
PatrickJS <github@gdi2290.com>
2018-08-08 18:49:22 -0700

    refactor: build to watch/build with shared

--- build/build.js
+++ build/build.js
@@ -1,34 +1,13 @@
-const fs = require('fs')
-const path = require('path')
-const rimraf = require('rimraf')
-
-const COMPONENTS_DIR = path.resolve(__dirname, '../src/components')
-const TMP_DIR = path.resolve(__dirname, 'tmp')
+const {
+  componentFileNames,
+  cleanTmp,
+  generateFile,
+  writeFile
+} = require('./shared')
 
 // Clean tmp directory
-rimraf.sync(TMP_DIR)
-fs.mkdirSync(TMP_DIR)
+cleanTmp()
 
 // create the main file for webpack
-const componentFileNames = fs.readdirSync(COMPONENTS_DIR)
-
-let output = ''
-
-// Styles
-output += "import '@/styles/globals'\n"
-
-// Components
-const printLine = filename =>
-  `export { default as Tipe${filename} } from '@/components/${filename}'\n`
-
-output += componentFileNames.reduce((string, filename) => {
-  return string + printLine(filename)
-}, '')
-
-fs.writeFile(path.resolve(TMP_DIR, 'index.js'), output, function(err) {
-  if (err) {
-    return console.log(err)
-  }
-
-  process.exit()
-})
+const output = generateFile(componentFileNames)
+writeFile(output)
