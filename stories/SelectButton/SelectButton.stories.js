617e65f47d26347b1d9f01c879dca7ae2b0923f8
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-10 10:13:48 -0700

    SelectButton

--- stories/SelectButton/SelectButton.stories.js
+++ stories/SelectButton/SelectButton.stories.js
@@ -1,85 +1,53 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, object, select } from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeSelect from '../../src/components/Select'
 
-const options = () => [
+const options = [
   { label: 'Owner', value: 'owner' },
   { label: 'Admin', value: 'admin' },
   { label: 'Member', value: 'member' }
 ]
-const style = () => ({
-  width: '300px'
-})
+
+const notes = `
+#### :props
+:options
+#### @events
+@change(value)
+`
+
 storiesOf('Select Button', module)
-  .add('default', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button"/></div>'
-  }))
-  .add('with placeholder', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" placeholder="Select a role..." /></div>'
-  }))
-  .add('default selected value', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" :defaultSelected="options[1]" type="button"" /></div>'
-  }))
-  .add('disabled', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" :disabled="true" /></div>'
-  }))
-  .add('success', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" color="success"/></div>'
-  }))
-  .add('danger', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select  :options="options" type="button" color="danger" /></div>'
-  }))
-  .add('warning', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" color="warning"/></div>'
-  }))
-  .add('waiting', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" :waiting="true"/></div>'
-  }))
-  .add('mini', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" size="mini"/></div>'
-  }))
-  .add('small', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" size="small"/></div>'
-  }))
-  .add('large', () => ({
-    components: { TipeSelect },
-    computed: { options, style },
-    template:
-      '<div :style="style"><tipe-select :options="options" type="button" size="large"/></div>'
-  }))
-  .add('no options', () => ({
-    components: { TipeSelect },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-select  type="button" size="large"/></div>'
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeSelect },
+      data() {
+        return {
+          style: { width: '300px' },
+          options: object('options', options),
+          defaultSelected: object('defaultSelected', options[0]),
+          color: select(
+            'color',
+            ['primary', 'info', 'none', 'success', 'danger', 'warning'],
+            'primary'
+          )
+        }
+      },
+      methods: {
+        change: action('change')
+      },
+      template: `
+        <div :style="style">
+          <tipe-select
+            @change="change"
+            :options="options"
+            :defaultSelected="defaultSelected"
+            :color="color"
+            type="button"
+          />
+        </div>`
+    }))
+  )
