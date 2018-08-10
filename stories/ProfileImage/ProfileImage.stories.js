824aa84363f79026580f0d1cf580bbe4defb8e4d
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-09 17:17:26 -0700

    ProfileImage: docs

--- stories/ProfileImage/ProfileImage.stories.js
+++ stories/ProfileImage/ProfileImage.stories.js
@@ -1,13 +1,28 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, text } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeProfileImage from '../../src/components/ProfileImage'
 
+const notes = `
+#### :props
+:url | string |
+`
+
 storiesOf('Profile Image', module)
-  .add('no profile image', () => ({
-    components: { TipeProfileImage },
-    template: '<tipe-profile-image />'
-  }))
-  .add('has profile image', () => ({
-    components: { TipeProfileImage },
-    template: '<tipe-profile-image url="http://placekitten.com/200/300" />'
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'has profile image',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeProfileImage },
+      data() {
+        return {
+          url: text('url', 'http://placekitten.com/200/300')
+        }
+      },
+      template: `
+      <tipe-profile-image
+        :url="url"
+      />`
+    }))
+  )
