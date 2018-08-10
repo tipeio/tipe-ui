7902918c4c0a914713abd0b11e123c6d4c6a95ad
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-10 11:00:28 -0700

    Switch: docs

--- stories/Switch/Switch.stories.js
+++ stories/Switch/Switch.stories.js
@@ -1,74 +1,63 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeSwitch from '../../src/components/Switch'
 
-const style = () => ({
-  width: '250px'
-})
-storiesOf('Switch', module)
-  .add('default', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing"/></div>'
-  }))
-  .add('without label', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template: '<div :style="style"><tipe-switch name="sharing"/></div>'
-  }))
-  .add('disabled', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :disabled="true"/></div>'
-  }))
-  .add('waiting', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :waiting="true"/></div>'
-  }))
-  .add('checked', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true"/></div>'
-  }))
-  .add('not tabable', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" :tabindex="-1"/></div>'
-  }))
-  .add('error', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" status="error"/></div>'
-  }))
+const notes = `
+#### :props
+:label | string |
+:name | string |
+:value | boolean |
+:status | status | '', success, warning, error
+:disabled | boolean |
+:waiting | boolean |
 
-  .add('warning', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" status="warning"/></div>'
-  }))
-  .add('success', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" status="success"/></div>'
-  }))
-  .add('small', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" size="small" /></div>'
-  }))
-  .add('large', () => ({
-    components: { TipeSwitch },
-    computed: { style },
-    template:
-      '<div :style="style"><tipe-switch label="File Sharing" name="sharing" :value="true" size="large"/></div>'
-  }))
+#### @events
+@click($event)
+@focus($event)
+@blur($event)
+@change($event)
+`
+
+storiesOf('Switch', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeSwitch },
+      data() {
+        return {
+          style: { width: '250px' },
+          label: text('label'),
+          name: text('name'),
+          value: boolean('value'),
+          status: select('status', ['', 'success', 'warning', 'error'], ''),
+          disabled: boolean('disabled', false),
+          waiting: boolean('waiting', false)
+        }
+      },
+      methods: {
+        click: action('click'),
+        focus: action('focus'),
+        blur: action('blur'),
+        change: action('change')
+      },
+      template: `
+        <div :style="style">
+          <tipe-switch
+            :label="label"
+            :name="name"
+            :value="value"
+            :status="status"
+            :disabled="disabled"
+            :waiting="waiting"
+            @click="click"
+            @focus="focus"
+            @blur="blur"
+            @change="change"
+          />
+        </div>`
+    }))
+  )
