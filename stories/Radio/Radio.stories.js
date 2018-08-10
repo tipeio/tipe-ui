5f1b27ce1172975a5c91fc228f55bcd2c35c1dfd
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-09 18:38:00 -0700

    Radio: docs

--- stories/Radio/Radio.stories.js
+++ stories/Radio/Radio.stories.js
@@ -1,54 +1,68 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, select, boolean } from '@storybook/addon-knobs/vue'
+import { action } from '@storybook/addon-actions'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeRadio from '../../src/components/Radio'
+import { text } from '../../node_modules/@storybook/addon-knobs/dist/base'
 
-const style = () => ({
-  width: '200px'
-})
+const note = `
+#### :props
+:checked | boolean |
+:size | string | mini, small, medium, large
+:status | string | success, warning, error
+:disabled | boolean |
+:waiting | waiting |
+:label | string |
+:name | string |
+
+@events
+@click($event)
+@focus($event)
+@blur($event)
+@change($event)
+`
 
 storiesOf('Radio', module)
-  .add('default radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" name="language" />'
-  }))
-  .add('small radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" size="small" />'
-  }))
-  .add('large radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" size="large"/>'
-  }))
-  .add('success radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" status="success"/>'
-  }))
-  .add('warning radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" status="warning"/>'
-  }))
-  .add('error radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" status="error"/>'
-  }))
-  .add('waiting radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" :waiting="true"/>'
-  }))
-  .add('disabled radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" :disabled="true"/>'
-  }))
-  .add('checked radio', () => ({
-    components: { TipeRadio },
-    computed: { style },
-    template: '<tipe-radio label="JavaScript" :checked="true"/>'
-  }))
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(note)(() => ({
+      components: { TipeRadio },
+      computed: {
+        style: { width: '200px' }
+      },
+      data() {
+        return {
+          checked: boolean('checked', false),
+          size: select('size', ['mini', 'small', 'medium', 'large'], 'medium'),
+          status: select('status', ['', 'success', 'warning', 'error'], ''),
+          disabled: boolean('disabled', false),
+          waiting: boolean('waiting', false),
+          label: text('label', ''),
+          name: text('name', '')
+        }
+      },
+      methods: {
+        click: action('click'),
+        focus: action('focus'),
+        blur: action('blur'),
+        change: action('change')
+      },
+      template: `
+        <tipe-radio
+          @click="click"
+          @focus="focus"
+          @blur="blur"
+          @change="change"
+          :value="value"
+          :checked="checked"
+          :size="size"
+          :status="status"
+          :disabled="disabled"
+          :waiting="waiting"
+          :label="label"
+          :name="language"
+        />`
+    }))
+  )
