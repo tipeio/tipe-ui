b8bb45fa943f7fddb5597f9b1f6412cfd63a39f0
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-09 17:23:50 -0700

    Progress: docs

--- stories/Progress/Progress.stories.js
+++ stories/Progress/Progress.stories.js
@@ -1,8 +1,31 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, number } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 
 import TipeProgress from '../../src/components/Progress'
 
-storiesOf('Progress', module).add('default', () => ({
-  components: { TipeProgress },
-  template: '<tipe-progress value="65" />'
-}))
+const notes = `
+#### :props
+:value | number
+:max | number
+`
+
+storiesOf('Progress', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeProgress },
+      data() {
+        return {
+          value: number('value', 56),
+          max: number('max', 100)
+        }
+      },
+      template: `
+        <tipe-progress
+          :value="value"
+          :max="max"
+        />`
+    }))
+  )
