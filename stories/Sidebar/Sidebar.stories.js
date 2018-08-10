ac33b776e8ecc9c14a7f469823bee1e5e1f00a05
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-10 10:23:58 -0700

    Sidebar: docs

--- stories/Sidebar/Sidebar.stories.js
+++ stories/Sidebar/Sidebar.stories.js
@@ -1,23 +1,42 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, object } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
 import TipeSidebar from '../../src/components/Sidebar'
 import { mocks } from '@tipe/tipe-test-utils'
 
-storiesOf('Sidebar', module).add('default', () => ({
-  components: { TipeSidebar },
-  data() {
-    return {
-      links: [
-        mocks.navLink({ icon: 'document' }),
-        mocks.navLink({ icon: 'folder' }),
-        mocks.navLink({ icon: 'media' }),
-        mocks.navLink({ icon: 'gear' })
-      ]
-    }
-  },
-  computed: {
-    styleObj: () => ({
-      height: '500px'
-    })
-  },
-  template: '<div :style="styleObj"><tipe-sidebar :links="links" /></div>'
-}))
+const notes = `
+#### :props
+:links | interfaces.navLink |
+`
+
+const links = [
+  mocks.navLink({ icon: 'document' }),
+  mocks.navLink({ icon: 'folder' }),
+  mocks.navLink({ icon: 'media' }),
+  mocks.navLink({ icon: 'gear' })
+]
+
+storiesOf('Sidebar', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withMarkdownNotes(notes)(() => ({
+      components: { TipeSidebar },
+      data() {
+        return {
+          links: object('link', links)
+        }
+      },
+      computed: {
+        styleObj: () => ({
+          height: '500px'
+        })
+      },
+      template: `
+        <div :style="styleObj">
+          <tipe-sidebar
+            :links="links"
+          />
+        </div>`
+    }))
+  )
