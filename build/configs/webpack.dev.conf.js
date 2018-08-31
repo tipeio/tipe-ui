e86f96a21ea40663c5418a581ad3cb6ea74b0d7c
Gary <gary.thomas.ryan@gmail.com>
2018-08-31 11:57:38 -0700

    fix: production build, extracts css, svg and uglyfies js

--- build/configs/webpack.dev.conf.js
+++ build/configs/webpack.dev.conf.js
@@ -3,5 +3,21 @@ const base = require('./webpack.base.conf')
 
 module.exports = merge(base, {
   externals: ['vue', 'vue-types', '@type/type-config', '@tipe/tipe-constants'],
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-sprite-loader'
+      }
+    ]
+  }
 })
